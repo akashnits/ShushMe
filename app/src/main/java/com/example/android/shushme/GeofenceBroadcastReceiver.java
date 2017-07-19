8408abf64761a78e9b39e1aa8478cf4f2eb0001e
Akash Raj <Akash.Raj@Lnttechservices.com>
2017-07-19 15:46:37 +0530

    Solution - 5

--- app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/android/shushme/GeofenceBroadcastReceiver.java
@@ -16,10 +16,16 @@ package com.example.android.shushme;
 * limitations under the License.
 */
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
 import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
 
@@ -37,12 +43,34 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.i(TAG, "onReceive called");
         // TODO (4) Use GeofencingEvent.fromIntent to retrieve the GeofencingEvent that caused the transition
+        GeofencingEvent gEvent=  GeofencingEvent.fromIntent(intent);
 
         // TODO (5) Call getGeofenceTransition to get the transition type and use AudioManager to set the
         // phone ringer mode based on the transition type. Feel free to create a helper method (setRingerMode)
 
+        int geofenceTransition= gEvent.getGeofenceTransition();
+
+        if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER){
+            setRingerMode(context, AudioManager.RINGER_MODE_SILENT);
+        }else if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL){
+            setRingerMode(context, AudioManager.RINGER_MODE_SILENT);
+        }else if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT)
+        {
+            setRingerMode(context, AudioManager.MODE_NORMAL);
+        }else {
+            Log.v(TAG, "Unknown Transition");
+
+        }
         // TODO (6) Show a notification to alert the user that the ringer mode has changed.
         // Feel free to create a helper method (sendNotification)
 
+    }
+
+    private void setRingerMode(Context context, int mode){
+        NotificationManager nm= (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT < 24 || (Build.VERSION.SDK_INT >= 24 && !nm.isNotificationPolicyAccessGranted())){
+            AudioManager audioManager= (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            audioManager.setRingerMode(mode);
+        }
     }
 }
